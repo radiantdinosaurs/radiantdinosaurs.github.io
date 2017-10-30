ee456ab3db937b5c78fe9f6c1281a94845ffaa9c
radiantdinosaurs <radiantdinosaurs@gmail.com>
2017-10-29 20:28:39 -0700

    Added in scrolling animation for links. Minor text fixes.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,18 @@
 $(document).ready(function(){
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+});
+
+$(document).ready(function(){
     $(window).on("scroll",function(){
         var wn = $(window).scrollTop();
         if(wn > 60){
