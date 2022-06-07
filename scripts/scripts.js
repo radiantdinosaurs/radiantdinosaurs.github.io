87d462782a5e35308f70d60b68f19df3c5f6ed88
Bethany Corder <radiantdinosaurs@gmail.com>
2022-06-06 22:09:16 -0700

    Renovates website

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,8 @@
+const LINE_BREAK = "<br />";
+
 $(document).ready(function() {
-    var app = document.getElementById("laptop-typewriter");
-    var typewriter = new Typewriter(app, {
+    const laptopScreenElement = document.getElementById("laptop-typewriter");
+    const typewriter = new Typewriter(laptopScreenElement, {
         delay: 75
     });
 
@@ -8,40 +10,39 @@ $(document).ready(function() {
         .typeString(
             'class <span class="syntax-purple">Human</span> <span class="curly-braces">{</span>'
         )
-        .typeString("<br>")
+        .typeString(LINE_BREAK)
         .typeString(
             '<span class="indent-once">constructor</span><span class="parentheses">(</span><span class="syntax-purple">name</span><span class="parentheses">)</span> <span class="curly-braces">{</span>'
         )
-        .typeString("<br>")
+        .typeString(LINE_BREAK)
         .typeString(
             '<span class="syntax-purple indent-twice">this</span>.name = name;'
         )
-        .typeString("<br>")
+        .typeString(LINE_BREAK)
         .typeString('<span class="indent-once curly-braces">}</span>')
-        .typeString("<br>")
-        .typeString("<br>")
+        .typeString(LINE_BREAK)
+        .typeString(LINE_BREAK)
         .pauseFor(500)
         .typeString(
             '<span class="indent-once">get</span> <span class="syntax-purple">greeting</span><span class="parentheses">()</span> <span class="curly-braces">{'
         )
-        .typeString("<br>")
+        .typeString(LINE_BREAK)
         .typeString(
-            '<span class="indent-twice syntax-purple">return</span> <span class="syntax-red">`Hello! My name is</span>'
+            '<span class="indent-twice syntax-purple">return</span> <span class="syntax-red">`Hello! I\'m</span> '
         )
-        .typeString("<br>")
         .typeString(
-            '<span class="indent-twice">$</span><span class="curly-braces">{</span>this.<span class="syntax-purple">name</span><span class="curly-braces">}</span>!<span class="syntax-red">`</span>;'
+            '$<span class="curly-braces">{</span>this.<span class="syntax-purple">name</span><span class="curly-braces">}</span>!<span class="syntax-red">`</span>;'
         )
-        .typeString("<br>")
-        .typeString('<span class="indent-once">}</span>')
-        .typeString("<br>")
-        .typeString("<br>")
+        .typeString(LINE_BREAK)
+        .typeString('<span class="indent-once curly-braces">}</span>')
+        .typeString(LINE_BREAK)
+        .typeString(LINE_BREAK)
         .typeString(
-            'const <span class="sytax-purple">Coder</span> = <span class="syntax-purple">new</span> Human<span class="parentheses">(</span><span class="syntax-red">"Bethany"</span><span class="parentheses">)</span>;'
+            'const <span class="syntax-purple">Coder</span> = <span class="syntax-purple">new</span> Human<span class="parentheses">(</span><span class="syntax-red">"Bethany"</span><span class="parentheses">)</span>;'
         )
-        .typeString("<br>")
-        .typeString("<br>")
-        .typeString("Coder.greeting;")
+        .typeString(LINE_BREAK)
+        .typeString(LINE_BREAK)
+        .typeString("Coder.greeting();")
         .start();
-    console.log("Hello! My name is Bethany!");
+    console.log("Hello! I'm Bethany!");
 });
